592e7bb45052c5eab996422aa8aac881a79b750a
JuliaMW <jmaria.wisniewski@gmail.com>
2014-10-19 11:06:06 +0200

    Correction de ma fonction JS
    parce que l'autre c'était de la merde!

--- war/script.js
+++ war/script.js
@@ -2,70 +2,89 @@
  * 
  */
 
-/*fonction qui vérifie les données saisies avant submit */
+/*fonctions qui vérifient les données saisies avant submit */
 function compareMail() {
-	var regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
-	var verif= 5;
 	var para= document.getElementById('notif');
 	
 	// vérif des mails
 	if (document.getElementById('email').value != document.getElementById('emailconf').value) {
 		para.innerHTML = 'Le mail et sa confirmation ne sont pas les mêmes.';
-	
+		return false;
 	}else{
 		para.innerHTML = 'Les champs marqués d\'une * sont obligatoires. Mot de passe: 8 caractères min.';
-		verif--;
+		return true;
 	}
+}
 	
-	
+function veriflogin(){	
 	//verif du login
 	if( (document.getElementById('Login').value.length<1)
 		|| (document.getElementById('Login').value.length >15)){
 		
 		document.getElementById('veriflogin').innerHTML= ' Login invalide';
-		
+		return false
 	}else{
 		document.getElementById('veriflogin').innerHTML= '';
-		verif--;
+		return true;
 	}
 	
+}
+
+function verifpassword(){
 	//verif du password
 	if(document.getElementById('Password').value.length <8){
 		
 		document.getElementById('verifpassword').innerHTML = ' Mot de passe invalide ';
-		
+		return false;
 	}else{
 		document.getElementById('verifpassword').innerHTML = '';
-		verif--;
+		return true;
 	}
 	
-	
-	//verif du mail
+}
+
+//verif du mail
+function verifmail(){
+	var regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
 	if((document.getElementById('email').value.length<1)
 		|| (!regex.test(document.getElementById('email').value))){
 		
 		document.getElementById('verifmail1').innerHTML = ' Mail invalide.';
-		
+		return false;
 	}else{
 		document.getElementById('verifmail1').innerHTML = '';
-		verif--;
+		return true;
 	}
 	
+}	
 	
-	//verif du mail
+//verif du mail
+function verifmail2(){
+	var regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
 	if((document.getElementById('emailconf').value.length<1) 
 		|| (!regex.test(document.getElementById('emailconf').value))){
 		
-		document.getElementById('verifmail').innerHTML = ' Mail invalide';		
+		document.getElementById('verifmail').innerHTML = ' Mail invalide';
+		return false;
 	}else{
 		document.getElementById('verifmail').innerHTML = '';
-		verif--;
+		return true;
 	}
 	
-	
+}
+
+function compare(){
 	//submit si tout est ok
-	if(verif==0)
+	var comparemailOK= compareMail();
+	var verifloginOk= veriflogin();
+	var verifpasswordOK=verifpassword();
+	var verifmailOk = verifmail2();
+	var verifmail2Ok= verifmail();
+	
+	if(comparemailOK && verifloginOk && verifpasswordOK && verifmailOk && verifmail2Ok)
 	{
 	document.getElementById('Register').submit();
 	}
 }
+	
+
