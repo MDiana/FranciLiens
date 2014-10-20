c426b05a5ca55803f38d150b67534d7cfc83e1a9
JuliaMW <jmaria.wisniewski@gmail.com>
2014-10-20 20:23:15 +0200

    Train BDD

--- src/franciliens/BackendServlet.java
+++ src/franciliens/BackendServlet.java
@@ -13,54 +13,59 @@ import javax.servlet.ServletException;
 
 @SuppressWarnings("serial")
 public class BackendServlet extends HttpServlet {
-	private static final Logger _logger = Logger.getLogger(BackendServlet.class.getName());
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-	throws IOException {
-
-	try {
-	_logger.info("Cronhas been executed");
-
-	//Put your logic here
-	//BEGIN
-	//END
-	}
-	catch (Exception ex) {
-	//Log any exceptions in your Cron Job
-	}
+	
+	
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
 	}
 
 	@Override
-	public void doPost(HttpServletRequest req, HttpServletResponse resp)
-	throws ServletException, IOException {
-	doGet(req, resp);
-	}
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.service(arg0, arg1);
 	}
 	
-//	@Override
-//	public void init() throws ServletException {
-//		// TODO Auto-generated method stub
-//		super.init();
-//	}
-//
-//	@Override
-//	protected void service(HttpServletRequest arg0, HttpServletResponse arg1)
-//			throws ServletException, IOException {
-//		// TODO Auto-generated method stub
-//		super.service(arg0, arg1);
-//	}
-//	
-//	@Override
-//	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-//			throws ServletException, IOException {
-//		// TODO Auto-generated method stub
-//		super.doGet(req, resp);
-//	}
-//	
-//	@Override
-//	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-//			throws ServletException, IOException {
-//		// TODO Auto-generated method stub
-//		super.doPost(req, resp);
-//	}
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+	
+	
 
-//}
+	//Exemple à laisser pour l'instant :)
+//		private static final Logger _logger = Logger.getLogger(BackendServlet.class.getName());
+//		public void doGet(HttpServletRequest req, HttpServletResponse resp)
+//		throws IOException {
+	//
+//		try {
+//		_logger.info("Cronhas been executed");
+	//
+//		//Put your logic here
+//		//BEGIN
+//		//END
+//		}
+//		catch (Exception ex) {
+//		//Log any exceptions in your Cron Job
+//		}
+//		}
+	//
+//		@Override
+//		public void doPost(HttpServletRequest req, HttpServletResponse resp)
+//		throws ServletException, IOException {
+//		doGet(req, resp);
+//		}
+//		}
+
+}
