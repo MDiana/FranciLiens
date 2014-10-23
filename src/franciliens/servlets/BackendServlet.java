b9c0322722d41903171f423e246334fcde6b84b5
JuliaMW <jmaria.wisniewski@gmail.com>
2014-10-23 16:14:47 +0200

    image + micro avancement du backend + ajoute librairie SAX

--- src/franciliens/servlets/BackendServlet.java
+++ src/franciliens/servlets/BackendServlet.java
@@ -2,6 +2,7 @@ package franciliens.servlets;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
@@ -12,13 +13,26 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
 import org.apache.commons.codec.binary.Base64;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+
+// pour le parsing utilisation de l'Api SAX car ne nécessite pas d'enregistrer l'aborescence en mémoire
+// "If the data structures have already been determined, and you are writing a server application or an XML filter that needs to do fast processing, see Simple API for XML." ORACLE
+// http://www.javablog.fr/javaxml-parsing-xml-with-jaxp-sax-dom-standard-apis.html 
+// http://java.developpez.com/faq/xml/?page=SAX#Comment-parser-un-XML-avec-SAX
 
 @SuppressWarnings("serial")
 public class BackendServlet extends HttpServlet {
-	
+
 	private static final Logger _logger = Logger.getLogger(BackendServlet.class.getName());
-	
+	private String currentElement;
+
 	@Override
 	public void init() throws ServletException {
 		// TODO connexion à l'API lors de l'initialisation
@@ -31,7 +45,7 @@ public class BackendServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(req, resp);
 	}
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
@@ -40,31 +54,33 @@ public class BackendServlet extends HttpServlet {
 		try{
 			_logger.setLevel(Level.INFO);
 			_logger.info("Nous allons accéder à l'api SNCF");
-		URL url = new URL("http://api.transilien.com/gare/87393009/depart/)");
-		URLConnection con = url.openConnection();
-		String login = "upmc107:xMcQ2p38";
-		String encodedLogin =new String(Base64.encodeBase64(login.getBytes()));
-		con.setRequestProperty("Authorization", "Basic " + encodedLogin);
-		//on utilise l'url comme un input non?
-		con.setDoInput(true);
-		//HttpURLConnection httpConn = (HttpURLConnection)con;
-		// Récupérer les trois prochains départs pour chaque gare (pour l'instant suffisant)
-		// Comme on ne peut faire que 30 requêtes par minutes, nous allons choisir 30 gares précises
-		/* Choix:
-		 * Aéroport Ch. de Gaulle 1 87271460
+			URL url = new URL("http://api.transilien.com/gare/87393009/depart/");
+			URLConnection con = url.openConnection();
+			String login = "upmc107:xMcQ2p38";
+			String encodedLogin =new String(Base64.encodeBase64(login.getBytes()));
+			con.setRequestProperty("Authorization", "Basic " + encodedLogin);
+			//on utilise l'url comme un input non?
+			con.setDoInput(true);
+
+
+			//HttpURLConnection httpConn = (HttpURLConnection)con;
+			// Récupérer les trois prochains départs pour chaque gare (pour l'instant suffisant)
+			// Comme on ne peut faire que 30 requêtes par minutes, nous allons choisir 30 gares précises
+			/* Choix:
+			 * Aéroport Ch. de Gaulle 1 87271460
 			Aéroport Ch. de Gaulle 2 TGV 87001479
 			Argenteuil 87381848
 			Aulnay sous Bois 87271411	
-		 *  Bellevue 87393116
-		 *  Bibliothèque François Mitterrand 87328328
-		 *  Bondy 87113407
-		 *  Châtelet les Halles 87758607
-		 *  Colombes 87381087
-		 *  Gennevilliers 87271205
-		 *  La Défense Grande Arche 87758011
-		 *  Les Mureaux 87386680
-		 *  Nanterre Université 87386318
-		 *  Paris Austerlitz 87547026
+			 *  Bellevue 87393116
+			 *  Bibliothèque François Mitterrand 87328328
+			 *  Bondy 87113407
+			 *  Châtelet les Halles 87758607
+			 *  Colombes 87381087
+			 *  Gennevilliers 87271205
+			 *  La Défense Grande Arche 87758011
+			 *  Les Mureaux 87386680
+			 *  Nanterre Université 87386318
+			 *  Paris Austerlitz 87547026
 			Paris Bercy 87686667
 			Paris Est 87113001
 			Paris Gare de Lyon 87686006
@@ -82,30 +98,45 @@ public class BackendServlet extends HttpServlet {
 			Villiers sur Marne Le Plessis Trévise 87113795
 			Parc des Expositions 87271486
 
-		 * 
-		 */
-		
-		_logger.info("Nous avons accedé à l'api SNCF");
-		BufferedReader in = new BufferedReader(new InputStreamReader(
-                con.getInputStream()));
-        String inputLine;
-        while ((inputLine = in.readLine()) != null)
-        	_logger.info(""+inputLine);
-        in.close();
-        
-		}catch(Exception e){
+			 * 
+			 */
+
+			_logger.info("Nous avons accedé à l'api SNCF");
+
+			// Create a SAX parser factory
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+
+			// Obtain a SAX parser
+			SAXParser saxParser = factory.newSAXParser();
+
+			// XML Stream
+			InputStream xmlStream = BackendServlet.class.getResourceAsStream("http://api.transilien.com/gare/87393009/depart/");
+
+			// Parse the given XML document using the callback handler
+			saxParser.parse(xmlStream, new XMLTrainHandler());
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
+		//		BufferedReader in = new BufferedReader(new InputStreamReader(
+		//                con.getInputStream()));
+		//        String inputLine;
+		//        while ((inputLine = in.readLine()) != null)
+		//        	_logger.info(""+inputLine);
+		//        in.close();
+		//        
 	}
-	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
-	}
-	
-	
+
+
+@Override
+protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+		throws ServletException, IOException {
+	// TODO Auto-generated method stub
+	super.doPost(req, resp);
+}
+
+
 //	Exemple à laisser pour l'instant :)
 //		private static final Logger _logger = Logger.getLogger(BackendServlet.class.getName());
 //		
@@ -130,6 +161,38 @@ public class BackendServlet extends HttpServlet {
 //		throws ServletException, IOException {
 //		doGet(req, resp);
 //		}
+
+
+class XMLTrainHandler extends DefaultHandler {
+
+	// Callback to handle element start tag
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		currentElement = qName;
+		if (currentElement.equals("passages")) {
+			String gare = attributes.getValue("gare");
+			_logger.info("Il s'agit de la gare: " + gare);
+		}
+	}
+
+	// Callback to handle element end tag
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		currentElement = "";
+	}
+
+	// Callback to handle the character text data inside an element
+	@Override
+	public void characters(char[] chars, int start, int length) throws SAXException {
+		if (currentElement.equals("train")) {
+			 _logger.info("C'est un train!!");
+
+		} else if (currentElement.equals("num")) {
+			 _logger.info("Le numero de ce train est le " + new String(chars, start, length));
+			
+		}
+	}
+}
 
 }
 
